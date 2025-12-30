500dc18d63580a3bdc89beb3404bc22db5a01576
JasonLi108 <jasonli108@gmail.com>
2025-12-29 21:01:39 -0500

    update vi to start ollama before loading nvim

--- functions/vi.fish
+++ functions/vi.fish
@@ -1,3 +1,6 @@
 function vi
+  # Start the model and tell it to stay loaded (-1)
+  curl -s -X POST http://localhost:11434/api/generate -d '{"model": "qwen2.5-coder:latest", "keep_alive": -1}' > /dev/null &
+
   nvim $argv
 end
