00dd77ccd8015e4f3f428ea63c56ebcca92f1c16
JasonLi108 <jasonli108@gmail.com>
2026-01-17 20:39:34 -0500

    add curl command to vi command to load ollama

--- functions/vi.fish
+++ functions/vi.fish
@@ -1,6 +1,6 @@
 function vi
   # Start the model and tell it to stay loaded (-1)
-  curl -s -X POST http://localhost:11434/api/generate -d '{"model": "qwen2.5-coder:latest", "keep_alive": -1}' > /dev/null &
+  curl -s -X POST http://localhost:11434/api/generate -d '{"model": "qwen2.5-coder:latest", "keep_alive": -1, "options": { "num_ctx": 8192 }}' > /dev/null &
 
   nvim $argv
 end
